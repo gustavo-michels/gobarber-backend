3254a4a72d6efd749a84ef3f27049a7b6c2a37e3
Gustavo Henrique Michels <ghmichels@gmail.com>
2019-08-14 18:36:46 -0300

    att

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '891b8160f94e50',
+    pass: '19a0a803b922bc',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
